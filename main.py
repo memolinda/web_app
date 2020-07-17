54092345bd56c3fa01a449861522fe5deeb427e4
memolinda <linda.giacomozzi@gmail.com>
2020-07-17 13:36:59 +0200

    main: set up the architecture to run the code

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 Builder.load_file("design.kv")
+
+class LoginScreen(Screen): #class with the same name as the rules in the .kv file
+    pass
+
+class RootWidget(ScreenManager):
+    pass
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+if __name__ =="__main__":
+    MainApp().run()
